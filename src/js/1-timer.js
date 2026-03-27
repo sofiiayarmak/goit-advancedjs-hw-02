cc9c9deed60bed7374cec4671dfa1a1014e4c638
Sofiia <sophia.yarmak@gmail.com>
2026-03-27 16:00:39 +0100

    timer js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -11,3 +11,67 @@ const timerFields = {
   minutes: document.querySelector('[data-minutes]'),
   seconds: document.querySelector('[data-seconds]'),
 };
+
+let userSelectedDate = null;
+let timerId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+
+    if (userSelectedDate < new Date()) {
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr(input, options);
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  input.disabled = true;
+
+  timerId = setInterval(() => {
+    const diff = userSelectedDate - new Date();
+
+    if (diff <= 0) {
+      clearInterval(timerId);
+      input.disabled = false;
+      return;
+    }
+
+    const time = convertMs(diff);
+    updateInterface(time);
+  }, 1000);
+});
+
+function updateInterface({ days, hours, minutes, seconds }) {
+  timerFields.days.textContent = String(days).padStart(2, '0');
+  timerFields.hours.textContent = String(hours).padStart(2, '0');
+  timerFields.minutes.textContent = String(minutes).padStart(2, '0');
+  timerFields.seconds.textContent = String(seconds).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
