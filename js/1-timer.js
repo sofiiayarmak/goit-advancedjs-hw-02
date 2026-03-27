4a678eb9fa8d9b5a010afe77814e377db9439320
Sofiia <sophia.yarmak@gmail.com>
2026-03-27 14:22:41 +0100

    fixed js library

--- js/1-timer.js
+++ js/1-timer.js
@@ -1,6 +1,24 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import flatpickr from 'flatpickr';
-const options = {};
-flatpickr('#datetime-picker', options);
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const daysVal = document.querySelector('[data-days]');
+const hoursVal = document.querySelector('[data-hours]');
+const minsVal = document.querySelector('[data-minutes]');
+const secsVal = document.querySelector('[data-seconds]');
+
+let userSelectedDate = null;
+let timerId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+  },
+};
